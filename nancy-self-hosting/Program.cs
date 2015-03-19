0ce1a3a06a66eec7ecae3a09d21b97e09b1ebcbc
witekcc <witekccc@yahoo.com>
2015-03-19 14:21:51 -0400

    initial working

---
+++ nancy-self-hosting/Program.cs
@@ -0,0 +1,46 @@
+﻿using nancy_self_hosting;
+//see: https://github.com/NancyFx/Nancy/wiki/Self-Hosting-Nancy
+
+namespace Nancy.Demo.Hosting.Self
+{
+    using System;
+    using System.Diagnostics;
+
+    using Nancy.Hosting.Self;
+
+    class Program
+    {
+
+        
+
+        static void Main()
+        {
+            HostConfiguration hostConfigs = new HostConfiguration()
+            {
+                UrlReservations = new UrlReservations() { CreateAutomatically = true }
+            };
+
+            using (var host = new NancyHost(new CustomBootstrapper(), new Uri("http://localhost:1234")))
+            {
+                host.Start();
+                Console.ReadLine();
+            }
+
+            /*
+            NancyHost nancyHost = new NancyHost(new Uri("http://localhost:8888"), new DefaultNancyBootstrapper(), hostConfigs);
+            
+            //var nancyHost = new NancyHost(hostConfigs, new Uri("http://localhost:8888/nancy/"), new Uri("http://127.0.0.1:8888/nancy/"), new Uri("http://localhost:8889/nancytoo/"));
+            //var nancyHost = new NancyHost(hostConfigs, new Uri("http://localhost:8854/nancy/"));
+            nancyHost.Start();
+
+            Console.WriteLine("Nancy now listening - navigating to http://localhost:8888/nancy/. Press enter to stop");
+            Process.Start("http://localhost:8888/nancy/");
+            Console.ReadKey();
+
+            nancyHost.Stop();
+
+            Console.WriteLine("Stopped. Good bye!");
+             * */
+        }
+    }
+}
---
+++ nancy-self-hosting/TestModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+
+public class MainModule : NancyModule
+{
+    public MainModule()
+    {
+        Get["/nancy"] = p => 
+            //"test2";
+            View["staticview"];
+    }
+}
+
+/*
+public class CustomRootPathProvider : IRootPathProvider
+{
+    public string GetRootPath()
+    {
+        return "What ever path you want to use as your application root";
+    }
+}
+*/
